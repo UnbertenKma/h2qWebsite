f778b57fbdc0dd5a509aea8c512441cdec91423c
UnbertenKma <mvthang01@gmail.com>
2023-07-04 11:17:20 +0700

    Add new pj

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LayoutWrapper from './screen/layouts';
+
+function App() {
+  return (
+    <div className="App">
+      <LayoutWrapper />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/screen/layouts/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from '../pages/Home';
+import Services from '../pages/Services';
+
+const LayoutWrapper = () => {
+  return (
+    <div>
+      <Home>Hello</Home>
+      <Services>Services</Services>
+    </div>
+  )
+}
+
+export default LayoutWrapper;
